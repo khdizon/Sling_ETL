a1289ad209acb0a7d6e14024f6da04bb03e8cf53
Ken H. Dizon <90869022+khdizon@users.noreply.github.com>
2024-04-09 13:53:16 -0600

    Update Sling_Package_Function.sql

--- Sling Functions/Sling_Package_Function.sql
+++ Sling Functions/Sling_Package_Function.sql
@@ -27,28 +27,47 @@
 
 
 -- Package Function # 2 Using All the Core + Add ons
+-- Churn by Entitlements
+
+SELECT
+    EXTRACT(YEAR FROM cal.clndr_dt) AS year,
+    EXTRACT(MONTH FROM cal.clndr_dt) AS month,
+    cal.clndr_dt AS date,
     CASE -- # Function: Package Details
         WHEN acnt_core_typ_cd IN (
             'DOMESTIC',
             'DOMESTIC/INTERNATIONAL',
             'DOMESTIC/LATINO',
-            'DOMESTIC/INTERNATIONAL/LATINO',
+            'DOMESTIC/INTERNATIONAL/LATINO'
         ) THEN 'Orange'
-        
         WHEN acnt_core_typ_cd IN (
             'DOMESTIC-MSS',
             'DOMESTIC-MSS/INTERNATIONAL',
             'DOMESTIC-MSS/LATINO',
-            'DOMESTIC-MSS/INTERNATIONAL/LATINO',
+            'DOMESTIC-MSS/INTERNATIONAL/LATINO'
         ) THEN 'Blue'
-        
         WHEN acnt_core_typ_cd IN (
             'DOMESTIC/DOMESTIC-MSS',
             'DOMESTIC/DOMESTIC-MSS/INTERNATIONAL',
             'DOMESTIC/DOMESTIC-MSS/LATINO',
-            'DOMESTIC/DOMESTIC-MSS/INTERNATIONAL/LATINO',
+            'DOMESTIC/DOMESTIC-MSS/INTERNATIONAL/LATINO'
         ) THEN 'Combo'
-
+                WHEN acnt_core_typ_cd IN (
+            'INTERNATIONAL/LATINO', 'INTERNATIONAL', 'LATINO'
+        ) THEN 'International/Latino'
         -- Other
-        ELSE 'Unknow'
+        ELSE 'Unknown'
     END AS package_name,
+    COUNT(acnt_sk) AS subscribers
+FROM 
+    edw_ott.ott_drvd_acnt_prfl AS acnt_profile
+    INNER JOIN dim.clndr_dim AS cal
+    ON cal.clndr_dt BETWEEN acnt_profile.bgn_eff_dt AND acnt_profile.endg_eff_dt
+        AND cal.clndr_dy_of_mnth = 1
+WHERE 
+    drvd_acnt_sts_cd IN ('Past Due','Cancel','Active') --# In place of cur_ind = 1
+    AND pyng_ind = 1 --# Paying customer
+    --AND acnt_core_typ_cd IN ('DOMESTIC-MSS', 'DOMESTIC/DOMESTIC-MSS', 'DOMESTIC')
+    AND cal.clndr_dt BETWEEN '01-01-2022' AND current_date
+GROUP BY 1,2,3,4
+ORDER BY 3, subscribers DESC
